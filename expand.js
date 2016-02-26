7e532fe3b73c051e871d05cb39b3699d68b4dadf
Dleavenworth <dleavenworth.ep@mysummitps.org>
2016-02-26 10:10:28 -0800

    Update expand.js

--- expand.js
+++ expand.js
@@ -1,12 +1,12 @@
 $(function() {
-    var mglass = $('.trigger');
+    var trigger = $('.trigger');
     var form = $('.header-search-box');
-    mglass.click(function() {
-        if (form.is(':hidden')) {
+    trigger.click(function() {
+        if(form.is(':hidden')) {
             form.show().focus()
         }
         form.animate({
-            'width': form.width() == 100 ? '0px' : '100px'
+            'width': form.width() == 200 ? '0px' : '200px'
         }, 'fast', function() {
             if (form.width() == 0) {
                 form.hide();
