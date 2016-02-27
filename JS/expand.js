2504e53d63073c07142ef44a73ab884ed6ee5aa0
David Leavenworth <dleavenworth.ep@mysummitps.org>
2016-02-26 18:54:28 -0800

    New file strucutre

--- JS/expand.js
+++ JS/expand.js
@@ -3,12 +3,12 @@ $(function() {
     var form = $('.form');
     trigger.click(function() {
         if(form.is(':hidden')) {
-            form.show().focus()
+            form.show().focus();
         }
         form.animate({
             'width': form.width() == 200 ? '0px' : '200px'
         }, 'fast', function() {
-            if (form.width() == 0) {
+            if (form.width() === 0) {
                 form.hide();
             }
         });
