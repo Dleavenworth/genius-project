a4097d7029cadce7ca6718d5b6f8317f4561a85e
Dleavenworth <dleavenworth.ep@mysummitps.org>
2016-03-18 10:38:27 -0700

    Added local storage

--- JS/agenda.js
+++ JS/agenda.js
@@ -2,5 +2,6 @@ var agenda = document.getElementById('agenda');
 var textarea = document.getElementById('textarea');
 
 $('#textarea').on('input', function() {
-  agenda.innerHTML = textarea.value;
+  localStorage.agenda = textarea.value;
+  agenda.innerHTML = localStorage.agenda;
 });
