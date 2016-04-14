95e3c0aa1a795a6483950a47c0fc9daf56fecaa3
David Leavenworth <dleavenworth.ep@mysummitps.org>
2016-04-13 19:38:49 -0700

    New styles?

--- scripts/components/Sidebar.js
+++ scripts/components/Sidebar.js
@@ -3,18 +3,18 @@ export default React.createClass({
   render() {
     return(
       <div className="left container z-depth-2">
-        <a href="#01" className="waves-effect waves-blue btn-flat">January</a>
-        <a href="#02" className="waves-effect waves-blue btn-flat">Febuary</a>
-        <a href="#03" className="waves-effect waves-blue btn-flat">March</a>
-        <a href="#04" className="waves-effect waves-blue btn-flat">April</a>
-        <a href="#05" className="waves-effect waves-blue btn-flat">May</a>
-        <a href="#06" className="waves-effect waves-blue btn-flat">June</a>
-        <a href="#07" className="waves-effect waves-blue btn-flat">July</a>
-        <a href="#08" className="waves-effect waves-blue btn-flat">August</a>
-        <a href="#09" className="waves-effect waves-blue btn-flat">September</a>
-        <a href="#10" className="waves-effect waves-blue btn-flat">October</a>
-        <a href="#11" className="waves-effect waves-blue btn-flat">November</a>
-        <a href="#12" className="waves-effect waves-blue btn-flat">December</a>
+        <a href="#01" className="waves-effect waves-red btn-flat">January</a>
+        <a href="#02" className="waves-effect waves-red btn-flat">Febuary</a>
+        <a href="#03" className="waves-effect waves-red btn-flat">March</a>
+        <a href="#04" className="waves-effect waves-red btn-flat">April</a>
+        <a href="#05" className="waves-effect waves-red btn-flat">May</a>
+        <a href="#06" className="waves-effect waves-red btn-flat">June</a>
+        <a href="#07" className="waves-effect waves-red btn-flat">July</a>
+        <a href="#08" className="waves-effect waves-red btn-flat">August</a>
+        <a href="#09" className="waves-effect waves-red btn-flat">September</a>
+        <a href="#10" className="waves-effect waves-red btn-flat">October</a>
+        <a href="#11" className="waves-effect waves-red btn-flat">November</a>
+        <a href="#12" className="waves-effect waves-red btn-flat">December</a>
       </div>
     );
   }
